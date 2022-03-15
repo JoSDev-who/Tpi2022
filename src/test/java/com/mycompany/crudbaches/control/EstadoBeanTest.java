8668dedab4fc13c895cc29442d92fd5fedb048f2
whoami <whoami@whoami-hp14notebookpc>
2022-03-15 10:00:55 -0600

    EstadoBanTest(solo crear y findByid)

--- src/test/java/com/mycompany/crudbaches/control/EstadoBeanTest.java
+++ src/test/java/com/mycompany/crudbaches/control/EstadoBeanTest.java
@@ -72,116 +72,115 @@ public class EstadoBeanTest {
 
     /**
      * Test of findAll method, of class EstadoBean.
-     */
-    @Test
-    public void testFindAll() throws Exception {
-        System.out.println("findAll");
-        EstadoBean cut = new EstadoBean();
-        assertThrows(IllegalStateException.class, () -> {
-            cut.findAll();
-        });
-        
-        EntityManager mockEM = Mockito.mock(EntityManager.class);
-        cut.em = mockEM;
-        cut.findAll();
-
-    }
-
-    /**
-     * Test of findRange method, of class EstadoBean.
-     */
-    @Test
-    public void testFindRange() throws Exception {
-        System.out.println("findRange");
-        int frist = 0;
-        int pageSize = 0;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        EstadoBean instance = (EstadoBean) container.getContext().lookup("java:global/classes/EstadoBean");
-        List<Estado> expResult = null;
-        List<Estado> result = instance.findRange(frist, pageSize);
-        assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of contar method, of class EstadoBean.
-     */
-    @Test
-    public void testContar() throws Exception {
-        System.out.println("contar");
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        EstadoBean instance = (EstadoBean) container.getContext().lookup("java:global/classes/EstadoBean");
-        Long expResult = null;
-        Long result = instance.contar();
-        assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of Modificar method, of class EstadoBean.
-     */
-    @Test
-    public void testModificar() throws Exception {
-        System.out.println("Modificar");
-        Estado nuevo = null;
-        long id = 0L;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        EstadoBean instance = (EstadoBean) container.getContext().lookup("java:global/classes/EstadoBean");
-        instance.Modificar(nuevo, id);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of Eliminar method, of class EstadoBean.
-     */
-    @Test
-    public void testEliminar() throws Exception {
-        System.out.println("Eliminar");
-        int id = 0;
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        EstadoBean instance = (EstadoBean) container.getContext().lookup("java:global/classes/EstadoBean");
-        instance.Eliminar(id);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getEntityManager method, of class EstadoBean.
-     */
-    @Test
-    public void testGetEntityManager() throws Exception {
-        System.out.println("getEntityManager");
-        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-        EstadoBean instance = (EstadoBean) container.getContext().lookup("java:global/classes/EstadoBean");
-        EntityManager expResult = null;
-        EntityManager result = instance.getEntityManager();
-        assertEquals(expResult, result);
-        container.close();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getEntityManager method, of class EstadoBean.
-     */
+//     */
 //    @Test
-//    public void testGetEntityManager() throws Exception {
-//        System.out.println("getEntityManager");
-//        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-//        EstadoBean instance = (EstadoBean)container.getContext().lookup("java:global/classes/EstadoBean");
-//        EntityManager expResult = null;
-//        EntityManager result = instance.getEntityManager();
-//        assertEquals(expResult, result);
-//        container.close();
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
+//    public void testFindAll() throws Exception {
+//        System.out.println("findAll");
+//        EstadoBean cut = new EstadoBean();
+//        assertThrows(IllegalStateException.class, () -> {
+//            cut.findAll();
+//        });
+//        
+//        EntityManager mockEM = Mockito.mock(EntityManager.class);
+//        cut.em = mockEM;
+//      
+//        
+//
 //    }
-//    
+//
+//    /**
+//     * Test of findRange method, of class EstadoBean.
+//     */
+//    @Test
+//    public void testFindRange() throws Exception {
+//        System.out.println("findRange");
+//        int frist = 0;
+//        int pageSize = 0;
+//        EstadoBean cut = new EstadoBean();
+//        assertThrows(IllegalStateException.class, () -> {
+//            cut.findRange(frist, pageSize);
+//        });
+//        
+//        EntityManager mockEM = Mockito.mock(EntityManager.class);
+//        cut.em = mockEM;
+//        cut.findRange(frist, pageSize);
+//    }
+//
+//    /**
+//     * Test of contar method, of class EstadoBean.
+//     */
+//    @Test
+//    public void testContar() throws Exception {
+//        System.out.println("contar");
+//        EstadoBean cut = new EstadoBean();
+//        assertThrows(IllegalStateException.class, () -> {
+//            cut.contar();
+//        });
+//        
+//        EntityManager mockEM = Mockito.mock(EntityManager.class);
+//        cut.em = mockEM;
+//        cut.contar();
+//    }
+//
+//    /**
+//     * Test of Modificar method, of class EstadoBean.
+//     */
+//    @Test
+//    public void testModificar() throws Exception {
+//        System.out.println("Modificar");
+//        long id = 0L;
+//        Estado nuevo = new Estado();
+//        EstadoBean cut = new EstadoBean();
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            cut.Modificar(null,id);
+//        });
+//        assertThrows(IllegalStateException.class, () -> {
+//            cut.Modificar(nuevo,id);
+//        });
+//        EntityManager mockEM = Mockito.mock(EntityManager.class);
+//        cut.em = mockEM;
+//        cut.Modificar(nuevo,id);
+//        //EJBContainer contain
+//    }
+//
+//    /**
+//     * Test of Eliminar method, of class EstadoBean.
+//     */
+//    @Test
+//    public void testEliminar() throws Exception {
+//        System.out.println("Eliminar");
+//        int id = 0;
+//        Estado nuevo = new Estado();
+//        EstadoBean cut = new EstadoBean();
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            cut.Eliminar(0);
+//        });
+//        assertThrows(IllegalStateException.class, () -> {
+//            cut.Eliminar(id);
+//        });
+//        EntityManager mockEM = Mockito.mock(EntityManager.class);
+//        cut.em = mockEM;
+//        cut.Eliminar(id);
+//    }
+//
+//    /**
+//     * Test of getEntityManager method, of class EstadoBean.
+//     */
+//
+//    /**
+//     * Test of getEntityManager method, of class EstadoBean.
+//     */
+////    @Test
+////    public void testGetEntityManager() throws Exception {
+////        System.out.println("getEntityManager");
+////        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
+////        EstadoBean instance = (EstadoBean)container.getContext().lookup("java:global/classes/EstadoBean");
+////        EntityManager expResult = null;
+////        EntityManager result = instance.getEntityManager();
+////        assertEquals(expResult, result);
+////        container.close();
+////        // TODO review the generated test code and remove the default call to fail.
+////        fail("The test case is a prototype.");
+////    }
+////    
 }
